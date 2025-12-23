c174b3ebea85b62d65c050bda541956303d80705
wesal <w.magdy2386@nu.edu.eg>
2025-12-23 10:11:34 +0200

    feat: added base gui files

---
+++ Ecommerceinventory/utils.py
@@ -0,0 +1,16 @@
+import mysql.connector
+import streamlit as st
+
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(
+            host=st.secrets["mysql"]["host"],
+            user=st.secrets["mysql"]["user"],
+            password=st.secrets["mysql"]["password"],
+            database=st.secrets["mysql"]["database"],
+            port=st.secrets["mysql"]["port"]
+        )
+        return conn
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return None
